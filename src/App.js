3a76a4c4537dc89d23a57b863f8d2672cb41d434
William Lowrimore <wlowrimore@gmail.com>
2022-11-12 22:57:23 -0600

    sets up functionality for dropdown toggle menu

--- src/App.js
+++ src/App.js
@@ -6,7 +6,26 @@ import AppointmentInfo from './components/AppointmentInfo';
 
 function App() {
 
-  const [appointmentList, setAppointmentList] = useState([]);
+  let [appointmentList, setAppointmentList] = useState([]);
+  let [query, setQuery] = useState('');
+  let [sortBy, setSortBy] = useState('petName');
+  let [orderBy, setOrderBy] = useState('asc');
+
+  const filteredAppointments = appointmentList.filter(
+    item => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+      )
+    }
+  ).sort((a, b) => {
+    let order = (orderBy === 'asc') ? 1 : -1;
+    return (
+      a[sortBy].toLowerCase() < b[sortBy].toLowerCase() 
+        ? -1 * order : 1 * order
+    )
+  })
 
   const fetchData = useCallback(() => {
     fetch('./data.json')
@@ -25,9 +44,16 @@ function App() {
       <h1 className='text-5xl mb-3'>
         <BiCalendar className='inline-block text-red-400 align-top' />Your Appointments</h1>
         <AddAppointment />
-        <Search />
+        <Search query={query}
+          onQueryChange={myQuery => setQuery(myQuery)}
+          orderBy={orderBy}
+          onOrderByChange={mySort => setOrderBy(mySort)}
+          sortBy={sortBy}
+          onSortByChange={mySort => setSortBy(mySort)}
+        />
+
         <ul className='divide-y divide-gray-200'>
-          {appointmentList
+          {filteredAppointments
             .map(appointment => (
               <AppointmentInfo key={appointment.id} 
                 appointment={appointment}
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,32 +1,36 @@
 import { useState } from 'react'
 import { BiCaretDown, BiCheck, BiSearch } from 'react-icons/bi'
 
-const DropDown = ({ toggle }) => {
+const DropDown = ({ toggle, sortBy, onSortByChange, orderBy, onOrderByChange }) => {
   if (!toggle) {
     return null;
   }
   return (
     <div className='origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5'>
-      <div
+      <div onClick={() => onSortByChange('petName')}
         className='px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between'
-        role='menuitem'>Pet Name <BiCheck /></div>
-        <div
+        role='menuitem'>Pet Name {(sortBy === 'petName') && <BiCheck />}</div>
+
+      <div onClick={() => onSortByChange('ownerName')}
         className='px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between'
-        role='menuitem'>Owner Name <BiCheck /></div>
-        <div
+        role='menuitem'>Owner Name {(sortBy === 'ownerName') && <BiCheck />}</div>
+
+      <div onClick={() => onSortByChange('aptDate')}
         className='px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between'
-        role='menuitem'>Date <BiCheck /></div>
-        <div
+        role='menuitem'>Date {(sortBy === 'aptDate') && <BiCheck />}</div>
+
+      <div onClick={() => onOrderByChange('asc')}
         className='px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between'
-        role='menuitem'>Asc <BiCheck /></div>
-        <div
+        role='menuitem'>Asc {(orderBy === 'asc') && <BiCheck />}</div>
+
+      <div onClick={() => onOrderByChange('desc')}
         className='px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between'
-        role='menuitem'>Desc <BiCheck /></div>
+        role='menuitem'>Desc {(orderBy === 'desc') && <BiCheck />}</div>
     </div>
   )
 }
 
-const Search = () => {
+const Search = ({ query, onQueryChange, sortBy, onSortByChange, orderBy, onOrderByChange }) => {
   const [toggleSort, setToggleSort] = useState(false);
 
   return (
@@ -36,7 +40,8 @@ const Search = () => {
           <BiSearch />
           <label htmlFor='query' className='sr-only' />
         </div>
-        <input type='text' name='query' id='query' value=''
+        <input type='text' name='query' id='query' value={query}
+                onChange={(e) => {onQueryChange(e.target.value)}}
                 className='pl-8 h-8 border rounded-md focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm'>
         </input>
         <div className='absolute inset-y-0 right-0 flex items-center'>
@@ -46,7 +51,12 @@ const Search = () => {
                     className='justify-center inline-flex px-2 py-1 rounded-sm bg-blue-400 border border-blue-400 hover:bg-blue-500  hover:border-blue-500 text-sm text-white font-normal'>
                     Sort By <BiCaretDown className='ml-2' />
             </button>
-            <DropDown toggle={toggleSort} />
+            <DropDown toggle={toggleSort}
+              sortBy={sortBy}
+              onSortByChange={mySort => onSortByChange(mySort)}
+              orderBy={orderBy}
+              onOrderByChange={myOrder => onOrderByChange(myOrder)}
+            />
           </div>
         </div>
       </div>
